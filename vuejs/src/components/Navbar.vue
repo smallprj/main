a2c036b0355a860a6e0eaad966775ff42a98ddbb
Nguyen Truong Trung Phuc <qwe_iop2006@yahoo.com>
2021-10-14 16:07:39 +0700

    Setup Scss

---
+++ vuejs/src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class='nav'>
+        <div class='nav__left'>
+            <span>Instructables picture</span>
+            <div>
+                <input type="Seach">
+            </div>
+        </div>
+        <div class='nav__right'>
+            <div>
+                <button>Log In</button>
+                <button>Register</button>
+            </div>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Category</a>
+                <a href="#">About</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'Navbar'
+}
+</script>
+
+<style lang="scss">
+.nav{
+    width:100%;
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
+    &__left{
+        display: flex;
+        flex-direction:row;
+    }
+    &__right{
+        display: flex;
+        flex-direction:row;
+    }
+}
+</style>
---
+++ vuejs/src/views/Test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>This is test page</p>
+        <Navbar />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'Test',
+  components: {
+    Navbar
+    }
+}
+</script>
